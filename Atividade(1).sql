0bf061ed3a1d1ef185e4aa7e5d65186a1244ef79
Rai123100 <165704080+Rai123100@users.noreply.github.com>
2024-10-21 14:46:42 -0300

    Terminando de deixar bunitinho

--- Atividade(1).sql
+++ Atividade(1).sql
@@ -1,76 +1,100 @@
---Quest„o 1
+--Quest√£o 1
 
 --Etapa A)
 
 --RESPOSTA: A quantidade de produtos continua igual.
 
---SELECT *
---FROM DimProduct
+SELECT
+ *
+FROM
+ DimProduct
 
 --Etapa B)
 
 --RESPOSTA: A quantidade de clientes diminuiu em 631.
 
---SELECT *
---FROM DimCustomer
+SELECT
+ *
+FROM
+ DimCustomer
 
 
---Quest„o 2
+--Quest√£o 2
 
 --Etapa A)
 
---SELECT CustomerKey, FirstName, EmailAddress, BirthDate
---FROM DimCustomer
+SELECT 
+ CustomerKey,
+ FirstName,
+ EmailAddress,
+ BirthDate
+FROM
+ DimCustomer
 
 --Etapa B)
 
---SELECT 
---CustomerKey AS 'ID do Cliente',
---FirstName AS Nome,
---EmailAddress AS 'E-mail',
---BirthDate AS 'Data de Nascimento'
---FROM DimCustomer
+SELECT 
+ CustomerKey AS 'ID do Cliente',
+ FirstName AS Nome,
+ EmailAddress AS 'E-mail',
+ BirthDate AS 'Data de Nascimento'
+FROM
+ DimCustomer
 
  
---Quest„o 3
+--Quest√£o 3
 
 --Etapa A)
 
---SELECT TOP (100) *
---FROM DimCustomer
+SELECT TOP (100)
+ *
+FROM
+ DimCustomer
 
 --Etapa B)
 
---SELECT TOP (20) PERCENT *
---FROM DimCustomer
+SELECT TOP (20) PERCENT
+ *
+FROM
+ DimCustomer
 
 --Etapa C)
 
---SELECT TOP (100) FirstName, EmailAddress, BirthDate
---FROM DimCustomer
+SELECT TOP (100)
+ FirstName,
+ EmailAddress,
+ BirthDate
+FROM
+ DimCustomer
 
 --Etapa D)
 
---SELECT 
---FirstName AS Nome,
---EmailAddress AS 'E-mail',
---BirthDate AS 'Data de Nascimento'
---FROM DimCustomer
+SELECT 
+ FirstName AS Nome,
+ EmailAddress AS 'E-mail',
+ BirthDate AS 'Data de Nascimento'
+FROM
+ DimCustomer
 
 
---Quest„o 4
+--Quest√£o 4
 
---SELECT DISTINCT
---Manufacturer AS Fornecedor
---FROM DimProduct
+SELECT DISTINCT
+ Manufacturer AS Fornecedor
+FROM
+ DimProduct
 
 
---Quest„o 5
+--Quest√£o 5
 
---SELECT Distinct ProductKey
---FROM FactSales
+SELECT Distinct
+ ProductKey
+FROM
+ FactSales
 
---SELECT DISTINCT ProductName
---FROM DimProduct
+SELECT DISTINCT
+ ProductName
+FROM
+ DimProduct
 
---RESPOSTA: 1 unico produto n„o foi vendido.
+--RESPOSTA: 1 unico produto n√£o foi vendido.
